74ddd6c48d3c26a547d0ac35f7fc35ee0c02380b
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-01 21:03:51 +0200

    Completed assignment

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name3 = args[2];
 		double sum = Double.parseDouble(args[3]);
 		sum = Math.ceil(sum/3.0);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + sum + " Shekels each");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + sum + " Shekels each");
 	}
 }
 
