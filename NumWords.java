74398c8503c92794a60ff7c4011a78ef31f475a3
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-01 22:15:27 +0200

    Completed assignment

--- NumWords.java
+++ NumWords.java
@@ -5,7 +5,7 @@ public class NumWords {
 		int number = Integer.parseInt(args[0]);
 		int ones = number % 10;
 		int tens = (number / 10) % 10;
-		int hundreds = (number / 100) % 10;
+		int hundreds = (number / 100);
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
