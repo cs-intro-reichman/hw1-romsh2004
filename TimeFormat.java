b0d69044a9c0f1547df0dfa4cd804ff7783693c4
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-01 22:41:37 +0200

    Completed assignment

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,6 +13,7 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         // Replace this comment with the rest of your code
+		System.out.println("% TimeFormat" + args[0]);
 		if (hours > 12)
 		{
 			hours = hours - 12;
